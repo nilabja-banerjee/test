0cf89dec3b71941ae2721a4c2a46f10ab6a46ddd
Nilabja Banerjee <nilabja@Nilabjas-MacBook-Pro.local>
2025-08-06 14:04:33 +0530

    clean up

--- ai_transformer/provider/base.py
+++
@@ -1,6 +0,0 @@
-class BaseAIProvider:
-    def __init__(self, config):
-        self.config = config
-
-    def generate_response(self, prompt, context):
-        raise NotImplementedError("Subclasses must implement this method")
